b20594d362e9ab1c2c425ce2e068ac931a4daab9
The Zux <thevupro753@gmail.com>
2024-09-13 14:05:39 +0700

    asdasd

--- src/pages/dmca/dmca.jsx
+++ src/pages/dmca/dmca.jsx
@@ -1,8 +1,11 @@
 
 const DMCA = (props) => {
   return (
+    <>
     <meta name='dmca-site-verification' content='UktSN0hUUFExeTlEbzJWbnBHd0poY2JZdWl1aGtxN1FxNXdILzI1Y2lVZVNOdmt0UE1IZG1TZ3dPakxuV0FxVnk5M0ZjRnNrNE9XcXdYL0pZeTRaamc9PQ2' />
-    
+    <div>
+        UktSN0hUUFExeTlEbzJWbnBHd0poY2JZdWl1aGtxN1FxNXdILzI1Y2lVZVNOdmt0UE1IZG1TZ3dPakxuV0FxVnk5M0ZjRnNrNE9XcXdYL0pZeTRaamc9PQ2
+    </div></>
   );
 };
 
